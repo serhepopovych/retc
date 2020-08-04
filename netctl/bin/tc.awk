serhepopovych/retc

commit 62a2364033ce742405a7966f016022f50338b20c

retc: Update submodule pointers for reipset and libusrxml
Update submodule pointer for reipset to 785ce5944cf4.
Update submodule pointer for libusrxml to 5f75a22c02c3.

With this submodule update utilities no longer use GNU coreutils
specific options so installation can be run with basic POSIX
environment. Helper alias-exec introduced to prepare environment
for running with multitool (e.g. busybox) provided utilities that
not installed system wide.

Adjust tc.awk to libusrxml.awk USRXML_users -> USRXML_ifnames
rename and use USRXML_ifuser instead of USRXML_ifnames to
interate over list of interfaces held by active users. This will
improve performance as now USRXML_ifnames may contain interface
descriptions.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/tc.awk
@@ -323,17 +323,18 @@ END{
 
 	u_if_nclassid = MINOR_CLASSID_MIN;
 
-	ifn = USRXML_ifnames[h,"num"];
+	ifn = USRXML_ifuser[h,"num"];
 	for (iff = 0; iff < ifn; iff++) {
-		if (!((h,iff) in USRXML_ifnames))
+		# Skip holes entries
+		if (!((h,iff) in USRXML_ifuser))
 			continue;
-		d_if = USRXML_ifnames[h,iff];
+		d_if = USRXML_ifuser[h,iff];
 
 		d_if_nclassid = MINOR_CLASSID_MIN;
 
 		d_if_has_qdisc = 0;
 
-		split(USRXML_ifnames[h,d_if], userids, " ");
+		split(USRXML_ifuser[h,d_if], userids, " ");
 		for (u in userids) {
 			# Uplink/Downlink class id limit reached: not adding class
 			if (u_if_nclassid > MINOR_CLASSID_MAX ||
@@ -386,7 +387,7 @@ END{
 				## TC
 
 				if (!ftc_user_printed) {
-					printf "\n### %s ###\n", USRXML_users[i] >>fclasses;
+					printf "\n### %s ###\n", USRXML_ifnames[i] >>fclasses;
 					ftc_user_printed = 1;
 				}
 
@@ -459,12 +460,12 @@ END{
 				init_client_ipset(fipset_in_v4,
 						  DFLT_MAJOR_CLASSID, minor_classid,
 						  "in", zones_in,
-						  i, USRXML_users, USRXML_usernets);
+						  i, USRXML_ifnames, USRXML_usernets);
 				# v6
 				init_client_ipset(fipset_in_v6,
 						  DFLT_MAJOR_CLASSID, minor_classid,
 						  "in", zones_in,
-						  i, USRXML_users, USRXML_usernets6);
+						  i, USRXML_ifnames, USRXML_usernets6);
 			}
 
 			# Out
@@ -475,12 +476,12 @@ END{
 				init_client_ipset(fipset_out_v4,
 						  DFLT_MAJOR_CLASSID, minor_classid,
 						  "out", zones_out,
-						  i, USRXML_users, USRXML_usernets);
+						  i, USRXML_ifnames, USRXML_usernets);
 				# v6
 				init_client_ipset(fipset_out_v6,
 						  DFLT_MAJOR_CLASSID, minor_classid,
 						  "out", zones_out,
-						  i, USRXML_users, USRXML_usernets6);
+						  i, USRXML_ifnames, USRXML_usernets6);
 			}
 		}
 	}

commit 8e4c4207eebc8b929af48983c1a0a71acbb5290f

retc: Use intermediate variable to store associative array key
This is minor optimization to eliminate multiple key calculations
on associative array dereferencing.

Fixes: 62a2364033ce ("retc: Update submodule pointers for reipset and libusrxml")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/bin/tc.awk
@@ -325,10 +325,14 @@ END{
 
 	ifn = USRXML_ifuser[h,"num"];
 	for (iff = 0; iff < ifn; iff++) {
+		# h,userid
+		i = h SUBSEP iff;
+
 		# Skip holes entries
-		if (!((h,iff) in USRXML_ifuser))
+		if (!(i in USRXML_ifuser))
 			continue;
-		d_if = USRXML_ifuser[h,iff];
+
+		d_if = USRXML_ifuser[i];
 
 		d_if_nclassid = MINOR_CLASSID_MIN;
 